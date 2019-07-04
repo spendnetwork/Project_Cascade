e0741afb51705ed08b829c77520cdc20ff9e9168
DMells <dave@davemellor.com>
2019-07-04 16:42:08 +1000

    Re-added clustering training and learned settings

--- Regions/UK_entities/Regional_Run_Files/data_matching.py
+++ Regions/UK_entities/Regional_Run_Files/data_matching.py
@@ -111,13 +111,14 @@ class Matching(Main):
             #             '--output_file',
             #             '/Users/davidmellor/Code/Spend_Network/Data_Projects/Project_Cascade/Regions/UK_entities/Outputs/Name_Only/Deduped_Data/Name_Only_matched.csv',
             #             '--skip_training']
-
             sys.argv = [
                         'csvlink',
                         str(src_file),
                         str(reg_df),
                         '--field_names_1', ' '.join(src_fields), '--field_names_2', ' '.join(reg_fields), '--training_file',
                         self.directories['manual_training_file'].format(self.region_dir, self.proc_type),
+                        '--sample_size', '500',
+                        '--settings_file','/Users/davidmellor/Code/Spend_Network/Data_Projects/Project_Cascade/learned_settings',
                         '--output_file',
                         self.directories['match_output_file'].format(self.region_dir, self.proc_type),
                         str(train[0])
