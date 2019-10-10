9735d0721e71cdd44244366d210530e19059f3c9
DMells <dave@davemellor.com>
2019-10-10 21:03:22 +0100

    Pytest - added AWS tests

---
+++ Regions/UK_entities/tests/test_AWS_calls.py
@@ -0,0 +1,34 @@
+from ..Regional_Run_Files import AWS_calls
+import pdb
+from dotenv import load_dotenv, find_dotenv
+import os
+import boto3
+
+# get the remote database details from .env
+load_dotenv(find_dotenv())
+aws_access_key_id = os.environ.get("aws_access_key_id")
+aws_secret_access_key = os.environ.get("aws_secret_access_key")
+
+def test_upload_file():
+    pass
+
+def test_processverifiedfiles():
+    pass
+
+def test_transfer(settings, adjust_default_args):
+
+    # Upload file
+    pdb.set_trace()
+    upload_file = os.path.join(settings.testdir, 'test_data', adjust_default_args.reg_adj)
+    bucket = 'org-matching'
+
+    AWS_calls.AwsTransfers.upload_file(upload_file, bucket, 'UK_entities/Tests/reg_data_adj_test.csv')
+
+    # Check file exists
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    response = s3.list_objects(Bucket=bucket, Prefix='UK_entities/Tests/')
+    file = response['Contents'][:]
+    assert file[0]['Key'] == 'UK_entities/Tests/reg_data_adj_test.csv'
+
+    # Delete file from s3
+    s3.delete_object(Bucket=bucket, Key=file[0]['Key'])
