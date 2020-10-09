6e5885ebd3b79163c521b645d24035df1e059a45
DMells <dave@davemellor.com>
2020-10-09 17:34:41 +0100

    Add UK Suppliers

--- settings.py
+++ settings.py
@@ -37,8 +37,8 @@ class UK_entities():
     reg_data_source = 'ocds.orgs_ocds'
     src_data_source = 'ocds.ocds_tenders_view'
     upload_table = 'matching.uk_entities'
-    transfer_table = 'matching.test_orgs_lookup'
-    # transfer_table = 'ocds.ocds_orgs_lookup'
+    # transfer_table = 'matching.test_orgs_lookup'
+    transfer_table = 'ocds.ocds_orgs_lookup'
 
     dedupe_cols = ['src_name','src_tag', 'src_name_adj', 'src_address_adj',
                    'reg_id', 'reg_name', 'reg_name_adj', 'reg_address', 'reg_address_adj','src_joinfields', 'reg_joinfields',
@@ -97,8 +97,8 @@ class UK_suppliers():
     reg_data_source = 'ocds.orgs_ocds'
     src_data_source = 'ocds.ocds_awards_suppliers_uk_view'
     upload_table = 'matching.uk_suppliers'
-    transfer_table = 'matching.test_orgs_lookup'
-    # transfer_table = 'ocds.ocds_orgs_lookup'
+    # transfer_table = 'matching.test_orgs_lookup'
+    transfer_table = 'ocds.ocds_orgs_lookup'
 
     dedupe_cols = ['src_name','src_tag', 'src_name_adj', 'src_address_adj',
                    'reg_id', 'reg_name', 'reg_name_adj', 'reg_address', 'reg_address_adj','src_joinfields', 'reg_joinfields',
